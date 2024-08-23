861710af8df84f2f1cdec3ef220a1e46e8b61596
LuieR <rhluisenrique@gmail.com>
2024-08-23 16:42:08 -0400

    chore: Implemented Routes protection for no authenticated users

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home-page/HomePage';
-import "./App.css"
+import "./App.css";
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import ProtectedRoute from './components/ProtectedRoutes';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />}/>
-        <Route path='/register' element={<Register />}/>
-        <Route path='/homepage/*' element={<HomePage />}/>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
 
-        <Route path='*' element={<Login />}/>
+        {/* Wrap the homepage route with ProtectedRoute */}
+        <Route 
+          path='/homepage/*' 
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Catch-all route */}
+        <Route path='*' element={<Login />} />
       </Routes>
     </>
   );
 }
+
 export default App;
+
